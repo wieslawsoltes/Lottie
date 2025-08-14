fd0542a2eae78d24e221c9c4ea62fc486e00a067
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-08-14 23:13:14 +0200

    Update Old.cs

--- samples/EffectsDemo/Old.cs
+++ samples/EffectsDemo/Old.cs
@@ -223,21 +223,7 @@ internal class DrawCompositionCustomVisualHandler
             ["iTime"] = _time,
             ["iMouse"] = new [] { 0f, 0f, -1f, -1f },
         };
-        
-        /*
-        uniform vec3      iResolution;           // viewport resolution (in pixels)
-        uniform float     iTime;                 // shader playback time (in seconds)
-        uniform float     iTimeDelta;            // render time (in seconds)
-        uniform float     iFrameRate;            // shader frame rate
-        uniform int       iFrame;                // shader playback frame
-        uniform float     iChannelTime[4];       // channel playback time (in seconds)
-        uniform vec3      iChannelResolution[4]; // channel resolution (in pixels)
-        uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
-        uniform samplerXX iChannel0..3;          // input channel. XX = 2D/Cube
-        uniform vec4      iDate;                 // (year, month, day, time in seconds)
-        uniform float     iSampleRate;           // sound sample rate (i.e., 44100)
-         */
-        
+
         // _children = new SKRuntimeEffectChildren(_effect) { ["iImage1"] = imageShader };
         _children = new SKRuntimeEffectChildren(_effect);
 
